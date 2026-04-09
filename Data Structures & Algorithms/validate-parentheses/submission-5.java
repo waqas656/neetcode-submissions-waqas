c1eb0a511eee5dd5c51f23232ea70fd72d1cd504
Waqas Ahmed <58516942+waqas656@users.noreply.github.com>
2026-04-09 15:05:25 +0300

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isValid(String s) {
+        // if string is empty then return true
+        // if string has only one character then return false
+        // there are only 3 types of brackets '{}', '()' and '[]'
+        // if bracket is not closing we push it to stack
+        // else if stack is not empty then :
+        // we determine whether bracket is among '}', ')' or ')' and then compare by checking last element in stack
+        // if bracket is other than this, return false
+        // else remove that bracket from stack
+        // after loop ends and if stack is not empty return false else true
+
+        Deque<Character> dq = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (isOpening(c)) dq.push(c);
+            else {
+                if (dq.isEmpty() || dq.pop() != partnerOf(c)) return false;
+            }
+        }
+
+        return dq.isEmpty();
+    }
+
+    private boolean isOpening(char c) {
+        if (c == '}' || c == ']' || c == ')') return false;
+        else return true;
+    }
+
+    private char partnerOf(char c) {
+        switch (c) {
+                case '}': return '{';
+                case ']': return '[';
+                case ')': return '(';
+                default:
+                return 'a';
+        }
+    }
+}
